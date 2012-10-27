e9a026d3622a508fa1aa297450490137365d682f
Nelson Morais <njmorais@proactive-hosters.com>
2012-10-27 15:42:13 +0100

    ...

---
+++ Soundcloud.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * SoundCloud API wrapper with support for authentication using OAuth 2
+ *
+ * @category  Services
+ * @package   Soundcloud
+ * @author    Nelson J Morais <njmorais@gmail.com>
+ * @copyright 2012 Nelson J Morais <njmorais@gmail.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://github.com/njasm/soundcloud
+ */
+
+Class Soundcloud 
+{
+    public function __construct() {
+        
+    }
+    
+    
+    public function __destruct() {
+        
+    }
+    
+}
+
+?>
