3459d332d6231f8fbaec6b3149eed645a518eb9a
Nelson Morais <njmorais@proactive-hosters.com>
2012-10-29 12:22:14 +0000

    first alpha version

---
+++ Soundcloud_Exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SoundCloud Exception (Unfinished!!!)
+ *
+ * @author      Nelson J Morais <njmorais@gmail.com>
+ * @copyright   2012 Nelson J Morais <njmorais@gmail.com>
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT
+ * @link        http://github.com/njasm/soundcloud
+ * @category    Services
+ * @package     Soundcloud Unfinished
+ * @todo        Get this class to 'alpha'usable' BETA version! ;)
+ */
+class Soundcloud_Exception extends Exception
+{
+    
+}
