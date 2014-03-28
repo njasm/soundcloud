6c129d1af884da6adaf1f9df0eca3b54659a402f
Nelson J Morais <njmorais@proactive-hosters.com>
2014-03-28 23:18:31 +0000

    added getUrl() assertion with params. to UrlBuilderTest.

--- tests/UrlBuilderTest.php
+++ tests/UrlBuilderTest.php
@@ -4,10 +4,14 @@ use Njasm\Soundcloud\UrlBuilder\UrlBuilder;
 
 class UrlBuilderTest extends \PHPUnit_Framework_TestCase 
 {
-    public function testGetUri()
+    public function testGetUrl()
     {
         // should return https://api.soundcloud.com/me
         $builder = new UrlBuilder("/me", array(), "get", "api");
         $this->assertEquals("https://api.soundcloud.com/me", $builder->getUrl());
+        
+        // should return https://api.soundcloud.com/tracks?q=buskers&license=cc-by-sa
+        $builder = new UrlBuilder("/tracks", array('q' => 'buskers', 'license' => 'cc-by-sa'), "get", "api");
+        $this->assertEquals("https://api.soundcloud.com/tracks?q=buskers&license=cc-by-sa", $builder->getUrl());        
     }
 }
