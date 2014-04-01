0b8ff6fdd26e7c8f0c55c868a48e10a233beaf77
Nelson João Morais <njasm@nelsons-imac.home>
2014-04-01 23:46:40 +0100

    Added Auth Class, Interface and Tests

---
+++ tests/AuthTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use Njasm\Soundcloud\Auth\Auth;
+
+class AuthTest extends \PHPUnit_Framework_TestCase 
+{
+    public function testException()
+    {
+        $this->setExpectedException(
+            '\InvalidArgumentException',
+            "No ClientID Provided."
+        );
+        $auth = new Auth();
+    }
+    
+    public function testSetAndGetClientID()
+    {
+        $auth = new Auth("ClientIDHash");
+        $this->assertEquals("ClientIDHash", $auth->getClientID());
+    }
+
+    public function testSetAndGetClientSecret()
+    {
+        $auth = new Auth("ClientIDHash", "ClientSecretHash");
+        $this->assertEquals("ClientSecretHash", $auth->getClientSecret());
+    }
+
+    public function testSetAndGetAuthUrlCallback()
+    {
+        $auth = new Auth("ClientIDHash", null, "http://api.soundcloud.com");
+        $this->assertEquals("http://api.soundcloud.com", $auth->getAuthUrlCallback());
+    }
+        
+    public function testSetAndGetToken()
+    {
+        $auth = new Auth("ClientIDHash");
+        $auth->setToken("Big_Secret_Token");
+        $this->assertEquals("Big_Secret_Token", $auth->getToken());     
+    }
+
+    
+    public function testSetAndExpires()
+    {
+        $auth = new Auth("ClientIDHash");
+        $auth->setExpires("1234567");
+        $this->assertEquals("1234567", $auth->getExpires());     
+    }    
+    
+    public function testSetAndScope()
+    {
+        $auth = new Auth("ClientIDHash");
+        $auth->setScope("*");
+        $this->assertEquals("*", $auth->getScope());     
+    }  
+    
+    public function testSetAndGetRefreshToken()
+    {
+        $auth = new Auth("ClientIDHash");
+        $auth->setRefreshToken("1-3456-asfaSy5hhjsWE");
+        $this->assertEquals("1-3456-asfaSy5hhjsWE", $auth->getRefreshToken());     
+    }
+}
