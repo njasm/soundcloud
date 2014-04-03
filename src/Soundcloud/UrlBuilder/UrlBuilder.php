f4af81cf3c986a204c574d686d78693b26bcbf1f
Nelson João Morais <njasm@nelsons-imac.home>
2014-04-04 00:22:55 +0100

    ...

--- src/Soundcloud/UrlBuilder/UrlBuilder.php
+++ src/Soundcloud/UrlBuilder/UrlBuilder.php
@@ -2,6 +2,8 @@
 
 namespace Njasm\Soundcloud\UrlBuilder;
 
+use Njasm\Soundcloud\Resource\ResourceInterface;
+use Njasm\Soundcloud\Auth\AuthInterface;
 
 class UrlBuilder implements UrlBuilderInterface
 {
@@ -9,25 +11,25 @@ class UrlBuilder implements UrlBuilderInterface
     private $scheme;
     private $hostname;
     private $subdomain;
-    private $verb;
-    private $path;
+    private $auth;
+    private $resource;
 
-    public function __construct(
-        $path = null, array $params = array(), $verb = "get", 
-        $subdomain = "api", $hostname = "soundcloud.com", $scheme = "https://")
+    public function __construct(ResourceInterface $resource, 
+                                AuthInterface $auth,
+                                $subdomain = "api", 
+                                $hostname = "soundcloud.com", 
+                                $scheme = "https://"
+                                )
     {
+        $this->resource = $resource;
+        $this->auth = $auth;
         $this->scheme = $scheme;
         $this->subdomain = $subdomain;
         $this->hostname = $hostname;
-        $this->path = $path;
-        $this->verb = $verb;
-        $this->setQuery($params);
+
     }
     
     
-    /**
-     * @return string uri query.
-     */
     public function getQuery()
     {
         return $this->query;
@@ -36,6 +38,7 @@ class UrlBuilder implements UrlBuilderInterface
     public function setQuery(array $params = array())
     {
         if (!empty($params)) {
+            $params = $this->mergeAuthParams($params);
             $this->query = http_build_query($params);
         } else {
             $this->query = null;
@@ -45,6 +48,7 @@ class UrlBuilder implements UrlBuilderInterface
     public function getUrl()
     {
         $url = $this->scheme . $this->subdomain . "." . $this->hostname;
+        $path = $this->getPath();
         $url .= ($this->path) ?: "/";
         
         if (strtolower($this->verb) == "get") {            
@@ -52,5 +56,27 @@ class UrlBuilder implements UrlBuilderInterface
         }
         
         return $url;
+    }
+    
+    public function mergeAuthParams(array $params = array()) 
+    {
+        $params = array_merge(
+            array(
+                'client_id' => $this->auth->getClientID()
+            ),
+            $this->resource->getParams()
+        );        
+    }
+    
+    public function getPath()
+    {
+        $path = $this->resource->getPath();
+        if (substr($path, strlen($path) - 1) == "/") {
+            $path = substr($path, 0, strlen($path) - 1);
+        } else {
+            $path .= "&client_id=" . $this->auth->getClientID();
+        }
+        
+        return $path;
     }
 }
--- tests/UrlBuilderTest.php
+++ tests/UrlBuilderTest.php
@@ -1,33 +1,39 @@
 <?php
 
 use Njasm\Soundcloud\UrlBuilder\UrlBuilder;
+use Njasm\Soundcloud\Resource\ResourceInterface;
+use Njasm\Soundcloud\Auth\Auth;
 
 class UrlBuilderTest extends \PHPUnit_Framework_TestCase 
 {
     public function testGetUrlWithoutParams()
     {
         // should return https://api.soundcloud.com/me
-        $builder = new UrlBuilder("/me", array(), "get", "api");
+        $auth = new Auth("ClientIDHash");
+        $builder = new UrlBuilder(Resource::get("/me"), $auth);
         $this->assertEquals("https://api.soundcloud.com/me", $builder->getUrl());
     }
     
     public function testGetGetUrl()
     {
         // should return https://api.soundcloud.com/tracks?q=buskers&license=cc-by-sa
-        $builder = new UrlBuilder("/tracks", array('q' => 'buskers', 'license' => 'cc-by-sa'), "get", "api");
+        $auth = new Auth("ClientIDHash");
+        $builder = new UrlBuilder(Resource::get("/me", array('q' => 'buskers', 'license' => 'cc-by-sa')), $auth);
         $this->assertEquals("https://api.soundcloud.com/tracks?q=buskers&license=cc-by-sa", $builder->getUrl());                
     }
     
     public function testPostGetUrl()
     {    
         // should return https://www.soundcloud.com/resolve
-        $builder = new UrlBuilder("/resolve", array('q' => 'john', 'license' => 'cc-by-sa'), "post", "www");
+        $auth = new Auth("ClientIDHash");
+        $builder = new UrlBuilder(Resource::get("/resolve", array('q' => 'john', 'license' => 'cc-by-sa')), $auth);
         $this->assertEquals("https://www.soundcloud.com/resolve", $builder->getUrl());          
     }
     
     public function testSetAndGetQuery()
     {
-        $builder = new UrlBuilder("/resolve", array('q' => 'john', 'license' => 'cc-by-sa'), "post", "www");
+        $auth = new Auth("ClientIDHash");
+        $builder = new UrlBuilder(Resource::get("/resolve", array('q' => 'john', 'license' => 'cc-by-sa')), $auth, "post", "www");
         $this->assertEquals("q=john&license=cc-by-sa", $builder->getQuery());
         
         $query = array(
@@ -39,7 +45,8 @@ class UrlBuilderTest extends \PHPUnit_Framework_TestCase
     
     public function testNullSetAndGetQuery()
     {
-        $builder = new UrlBuilder("/resolve", array(), "post", "www");
+        $auth = new Auth("ClientIDHash");        
+        $builder = new UrlBuilder("/resolve", array(), $auth, "post", "www");
         $this->assertNull($builder->getQuery());     
     }       
 }
