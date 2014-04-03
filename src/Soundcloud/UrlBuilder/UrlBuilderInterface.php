4f83a3c386b09a084bcfe58ce7b20f533e95589d
Nelson J Morais <njmorais@proactive-hosters.com>
2014-04-04 00:22:55 +0100

    ...

--- src/Soundcloud/UrlBuilder/UrlBuilderInterface.php
+++ src/Soundcloud/UrlBuilder/UrlBuilderInterface.php
@@ -4,22 +4,13 @@ namespace Njasm\Soundcloud\UrlBuilder;
 
 interface UrlBuilderInterface 
 {
-    
-    public function __construct($path = null, array $params = array(), $verb = "get", $subdomain = "api");    
+    public function __construct(ResourceInterface $resource, 
+                                AuthInterface $auth,
+                                $subdomain = "api", 
+                                $hostname = "soundcloud.com", 
+                                $scheme = "https://"
+                                );
+    public function setQuery(array $params = array());
     public function getQuery();
-    
-    /**
-     * Returns fully qualified URL.
-     * 
-     * @return string fully qualified url to query a soundcloud api resource
-     */
-    public function getUrl();
-    
-    /**
-     * Build http query.
-     * 
-     * @param array $params key value pair to build the query.
-     * @return void
-     */    
-    public function setQuery(array $params = array());    
+    public function getUrl();    
 }
