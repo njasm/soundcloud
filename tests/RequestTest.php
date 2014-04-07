32c7110ed847dea4a2c3e18ce811f75eff0a4c1a
njasm <njasm@nelsons-imac.home>
2014-04-07 22:08:42 +0100

    readme

---
+++ tests/RequestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Njasm\Soundcloud\Request\Request;
+use Njasm\Soundcloud\UrlBuilder\UrlBuilder;
+use Njasm\Soundcloud\Resource\Resource;
+
+
+class RequestTest extends \PHPUnit_Framework_TestCase 
+{
+    public function testNothing()
+    {
+        
+    }
+}
