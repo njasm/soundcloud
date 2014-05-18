9e5aa2b0f806cb68193dad34cfe445fcdf8dad59
njasm <njmorais@proactive-hosters.com>
2014-05-18 03:23:19 +0100

    Readme changes. added download() method. to Soundcloude Facade.

--- src/Soundcloud/Request/ResponseInterface.php
+++ src/Soundcloud/Request/ResponseInterface.php
@@ -4,9 +4,40 @@ namespace Njasm\Soundcloud\Request;
 
 interface ResponseInterface
 {
+    /**
+     * Get Response Headers
+     * 
+     * @return Array the headers as key => value pairs.
+     */
     public function getHeaders();
+    
+    /**
+     * Return header
+     * 
+     * @param string $header the asked header
+     * @return string|null the header string, if set, null otherwise
+     */
     public function getHeader($header);
+    
+    /**
+     * Check if header exists/is set.
+     * 
+     * @param type $header the asked header
+     * @return Boolean true if exists, false otherwise
+     */
     public function hasHeader($header);
+    
+    /**
+     * Return the body response in raw string format.
+     * 
+     * @return string the last body response
+     */
     public function bodyString();
+    
+    /**
+     * Returns an object based on the Content-Type of the response.
+     * 
+     * @return Object 
+     */
     public function bodyObject();
 }
