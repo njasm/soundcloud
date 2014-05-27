c341f1a06597b8e0c434ca554f1eba63252d19bb
njasm <njmorais@proactive-hosters.com>
2014-05-28 00:25:59 +0100

    refactor Resource

--- src/Soundcloud/Resource/Resource.php
+++ src/Soundcloud/Resource/Resource.php
@@ -23,17 +23,17 @@ class Resource implements ResourceInterface
     
     public function __construct($verb = null, $path = null, array $params = array())
     {
-        $this->validate($verb);
+        $this->isValidVerb($verb);
         $this->verb = $verb;
         $this->params = $params;
         
-        if (is_string($path) && substr($path, 0, 1) === "/") {
-            $this->path = $path;
-        } else {
+        if ($this->isValidPath($path) === false) {
             throw new SoundcloudException(
                 "Path cannot be other then a string type and should start with a '/' (Slash)."
             );
         }
+        
+        $this->path = $path;
     }
     
     public function setParams(array $params = array())
@@ -58,10 +58,15 @@ class Resource implements ResourceInterface
         return $this->verb;
     }
     
-    private function validate($verb)
+    private function isValidVerb($verb)
     {
         if (in_array(strtolower($verb), $this->availableVerbs) === false) {
             throw new SoundcloudException("Resource of type: $verb, not available!");
         }
+    }
+    
+    private function isValidPath($path)
+    {
+        return is_string($path) === true && substr($path, 0, 1) === "/";
     }
 }
