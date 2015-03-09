f15b0a5e02b7608586d84c84521273a430364e07
Nelson Morais <njmorais@gmail.com>
2015-03-09 22:07:30 +0000

    added autoloader.php - related to #14 and #15

--- src/Soundcloud/autoload.php
+++ src/Soundcloud/autoload.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: njasm
- * Date: 09/03/15
- * Time: 21:56
+ * Autoloader provided for Njasm Soundcloud Library.
  */
+
+spl_autoload_register(
+    function ($class) {
+        $prefix = 'Njasm\\Soundcloud\\';
+        $base_dir = __DIR__ . '/';
+        $len = strlen($prefix);
+
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // move to the next registered autoloader
+            return;
+        }
+
+        $relative_class = substr($class, $len);
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+);
