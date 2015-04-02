b29f6dc0319ab23f11da77e620d71158c7684653
Nelson Morais <njmorais@gmail.com>
2015-04-03 00:14:04 +0100

    More ApiResponseFactory tests

--- tests/Factory/ApiResponseFactoryTest.php
+++ tests/Factory/ApiResponseFactoryTest.php
@@ -42,4 +42,33 @@ class ApiResponseFactoryTest extends \PHPUnit_Framework_TestCase
             ApiResponseFactory::resource(json_encode($value))
         );
     }
+
+    public function testResolve()
+    {
+        $data = "{\"status\": \"302 - Found\",\"location\": \"https://api.soundcloud.com/users/1492543?consumer_key=apigee\"}";
+        $expected = '\Njasm\Soundcloud\Resolve\Resolve';
+        $resolve = ApiResponseFactory::resolve($data);
+
+        $this->assertInstanceOf($expected, $resolve);
+
+        $resolve2 = ApiResponseFactory::unserialize($data);
+        $this->assertInstanceOf($expected, $resolve2);
+    }
+
+    public function testCollectionWithItems()
+    {
+        $data = '[{"kind": "comment", "id": 1}, {"kind": "comment", "id": 2}]';
+        $expectedCollection = '\Njasm\Soundcloud\Collection\CommentCollection';
+        $returnValue = ApiResponseFactory::unserialize($data);
+        $this->assertInstanceOf($expectedCollection, $returnValue);
+        $this->assertCount(2, $returnValue);
+    }
+
+    public function testUnserializeResource()
+    {
+        $data = include __DIR__ .'/Serialized_User.php';
+        $expected = '\Njasm\Soundcloud\Resource\User';
+        $returnValue = ApiResponseFactory::unserialize($data);
+        $this->assertInstanceOf($expected, $returnValue);
+    }
 }
